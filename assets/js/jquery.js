141ef11a2034c52fa3e97a3d319bfebb0765491c
RPVB <rpvbalen@gmail.com>
2014-09-21 16:29:13 +0200

    fixed errors

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,25 +1,35 @@
-$( document ).ready(function() {
-	for (var i = 0; i < 36; i++) {
-		$( "#main-container" ).append( "<div id='container" + i + "' class='container'>" + i + "</div>" );
-	};
+$(document).ready(function() {
+    for (var i = 0; i < 36; i++) {
+        $("#main-container").append("<div id='container" + i + "' class='container'>" + i + "</div>");
+    }
+    ;
 
-	$(".container").click(function() {
-          hmpps = $(this).touching();
-		random = Math.round(Math.random()* 10); 
-                if(random < 3 ){
-                    $.each(hmpps, function(key,valueObj){
-                       console.log(valueObj.getAttribute('id'));
-                    });
-                    $(this).removeClass('green').removeClass('red').removeClass('blue');
-                    $(this).addClass("red");
+    $(".container").click(function() {
+
+        random = Math.round(Math.random() * 10);
+        if (random < 3) {
+            $(this).removeClass('green').removeClass('red').removeClass('blue');
+            $(this).addClass("red");
+        }
+        if (random > 3 && random < 6) {
+            $(this).removeClass('green').removeClass('red').removeClass('blue');
+            $(this).addClass("green");
+        }
+        if (random > 6) {
+            $(this).removeClass('green').removeClass('red').removeClass('blue');
+            $(this).addClass("blue");
+        }
+        hmpps = $(this).touching();
+        console.log(hmpps);
+
+        $.each(hmpps, function(key, valueObj) {
+            var bordering = $(document.getElementById(valueObj.getAttribute('id')));
+            if ($(this).attr('id') != bordering.attr('id')) {
+                if ($(this).hasClass("red") && bordering.hasClass("red")) {
+                    console.log('kaboem klengreblankbam boem');
                 }
-                if(random > 3 && random < 6 ){
-                    $(this).removeClass('green').removeClass('red').removeClass('blue');
-                    $(this).addClass("green");
-                }
-                if(random > 6 ){
-                    $(this).removeClass('green').removeClass('red').removeClass('blue');
-                    $(this).addClass("blue");
-                }
-	});
+            }
+        });
+
+    });
 });
